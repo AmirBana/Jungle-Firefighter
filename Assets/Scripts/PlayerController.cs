fdd2a6711d1f7984df380b539d8c84df072e86c5
AmirBana <83809960+AmirBana@users.noreply.github.com>
2022-07-17 09:55:16 +0430

    got error

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float sizeMove;
     private float cameraSize;
     [SerializeField] float forwardSpeed = 1f;
     void Start()
     {
-        cameraSize = Camera.main.orthographicSize;
-        sizeMove = cameraSize / 3;
+
     }
     void Update()
     {
         transform.Translate(Vector3.up*forwardSpeed);
         if (Input.GetKey(KeyCode.A) )//todo add mobile swipe
         {
-            transform.Rotate(0, 0,1f);
+            transform.
+            transform.Rotate(0, -1f, 0, Space.World);
         }
         if (Input.GetKey(KeyCode.D))//todo add mobile swipe
         {
-            transform.Rotate(0,0, -1f);
+            transform.Rotate(0, 1f, 0, Space.World);
             //transform.Translate(Vector3.right);
         }
     }
