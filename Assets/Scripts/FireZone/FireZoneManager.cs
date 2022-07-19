ac900dbb12a7f55888eb54e5e3507b9ba3168897
AmirBana <83809960+AmirBana@users.noreply.github.com>
2022-07-19 12:46:01 +0430

    Zone generator
    there is a zone that fire is inside it

--- Assets/Scripts/FireZone/FireZoneManager.cs
+++ Assets/Scripts/FireZone/FireZoneManager.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class FireZoneManager : MonoBehaviour
 {
     [SerializeField] GameObject cube;
-    int size = 2;
-    int xBase = 0;
-    int zBase = 0;
+    float size = 2f;
+    float xBase ;
+    float zBase ;
+    float xMinBound, xMaxBound;
     int hHeight, vHeight;
     // Start is called before the first frame update
     void Start()
     {
-        size = (int)cube.transform.localScale.x;
+        size = cube.transform.localScale.x;
+        xBase = transform.position.x;
+        zBase = transform.position.z;
+        xMinBound = -19+size;
+        xMaxBound = 11-size;
+        print(xBase+' '+ zBase);
         print(size);
-        int zLines=Random.Range(3, 7);
+        int zLines=Random.Range(5, 15);
         int xLines = Random.Range(2, 5);
         print(xLines);
         print(zLines);
@@ -28,13 +34,48 @@ public class FireZoneManager : MonoBehaviour
     }
     void Spawner(int zLines,int xLines)
     {
-        for(int z=zBase;z<zLines* size; z+= size)
+        int fire = -1;
+        List<GameObject> line = new List<GameObject>();
+        for (float z=zBase;z<zBase+(zLines* size); z+= size)
         {
-            for(int x=xBase;x<xLines* size; x+= size)
+            for (float x = xBase; x < xBase + (xLines * size); x += size)
             {
-                Vector3 spawnPos = new Vector3(x,transform.position.y,z);
-                Instantiate(cube,spawnPos,cube.transform.rotation,transform);
+                Vector3 spawnPos = new Vector3(x, transform.position.y, z);
+                GameObject sCube=Instantiate(cube, spawnPos, cube.transform.rotation, transform);
+                line.Add(sCube);
             }
+            if (fire == -1)
+            {
+                fire = Random.Range(0, line.Count);
+                line[fire].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            else if (fire == 0)
+            {
+                fire = Random.Range(0, fire + 1);
+                line[fire].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            else if (fire == line.Count)
+            {
+                fire=Random.Range(fire - 1, fire);
+                line[fire].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            else
+            {
+                fire = Random.Range(fire - 1, fire + 2);
+                line[fire].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            line.Clear();
         }
+    }
+    void GeneratePath(float z)
+    {
+        float xS = xBase;
+        float[] x = new float[3];
+        x[0] = xS;
+        x[1] = xS + size;
+        x[2] = xS - size;
+        xS = x[Random.Range(0, 3)];
+        Vector3 spawnPos = new Vector3(xS, transform.position.y, z);
+        Instantiate(cube, spawnPos, cube.transform.rotation, transform);
     }
 }
