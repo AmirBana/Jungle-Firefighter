d52cb2f86458e50d790a8a3b46097f7686656791
AmirBana <83809960+AmirBana@users.noreply.github.com>
2022-07-26 18:53:46 +0430

    map generator cube scaler

--- Assets/Scripts/map/MapGenerator.cs
+++ Assets/Scripts/map/MapGenerator.cs
@@ -17,26 +17,44 @@ public class MapGenerator : MonoBehaviour
         xGrid = xGrid*size;
         zGrid = zGrid * size;
         isOdd = xGrid%2==0 ? false : true;
+        CubeScale();
         Generator();
+    }
+    private void CubeScale()
+    {
+        baseCubes[0].transform.localScale = new Vector3(xGrid+20, 1, 10);
+        baseCubes[1].transform.localScale = new Vector3(xGrid+20, 1, 10);
     }
     private void Update()
     {
         transform.Translate(-Vector3.forward * speed * Time.deltaTime);
     }
-    void Generator()
+    /*void Generator()
     {
        
         int startCube = 0;
         Vector3 sPos;
-        for(int z = 0;z < zGrid;z += size)
+        for(int z = -10;z < zGrid+30;z += size)
         {
-            for(int x = 0;x < xGrid;x += size)
+            for(int x = -10;x < xGrid+10;x += size)
             {
                 sPos = new Vector3(transform.position.x+x,transform.position.y,transform.position.z+z);
-                GameObject cell=Instantiate(baseCubes[startCube], sPos, transform.rotation, transform);
+                Instantiate(baseCubes[startCube], sPos, transform.rotation, transform);
                 startCube = startCube == 1 ? 0 : 1;
             }
             if(!isOdd)
+                startCube = startCube == 1 ? 0 : 1;
+        }
+    }*/
+    void Generator()
+    {
+        int startCube = 0;
+        Vector3 sPos;
+        int x =-10;
+        for (int z = -10; z < zGrid + 3; z += 10)
+        {
+                sPos = new Vector3(transform.position.x+5, transform.position.y, transform.position.z + z);
+                Instantiate(baseCubes[startCube], sPos, transform.rotation, transform);
                 startCube = startCube == 1 ? 0 : 1;
         }
     }
